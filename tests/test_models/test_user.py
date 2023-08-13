6813080edb29f2feaf8d3b8161ca85eb98c7d03b
saaid <saaid2538@gmail.com>
2023-08-13 05:12:22 -0700

    added test_user.py

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import unittest
+from models.user import User
+
+class TestUser(unittest.TestCase):
+    def test_user_attributes(self):
+        user = User()
+        self.assertTrue(hasattr(user, "email"))
+        self.assertTrue(hasattr(user, "password"))
+        self.assertTrue(hasattr(user, "first_name"))
+        self.assertTrue(hasattr(user, "last_name"))
+
+if __name__ == '__main__':
+    unittest.main()
+
