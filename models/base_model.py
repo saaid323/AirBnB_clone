a797066e8814495b7bf2d1deabb83ce2ff17db00
saaid <saaid2538@gmail.com>
2023-08-12 23:42:10 -0700

    edited base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -13,13 +13,13 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """*args, **kwargs arguments for the constructor of a BaseModel"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.created_at = datetime.now()
         self.updated_at = self.created_at
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if not k == "__class__":
                     if k == "created_at" or k == "updated_at":
-                        v = datetime.now()
+                        v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                         setattr(self, k, v)
                     setattr(self, k, v)
         else:
@@ -32,14 +32,14 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at with the current
         datetime"""
-        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of
         the instance"""
-        data = self.__dict__
+        data = self.__dict__.copy()
         data["__class__"] = self.__class__.__name__
-        data["updated_at"] = self.updated_at
-        data["created_at"] = self.created_at
+        data["updated_at"] = self.updated_at.isoformat()
+        data["created_at"] = self.created_at.isoformat()
         return data
